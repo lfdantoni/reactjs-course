17facb80f328f4b84baf8650e7f1386e2ec82e65
lfdantoni <lfdantoni@gmail.com>
2019-03-12 19:43:02 -0300

    Added reactjs vanilla example project

---
+++ lesson-1/react-vanilla/webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const HtmlPlugin = new HtmlWebpackPlugin({
+    template : __dirname + "/index.html",
+    filename : "index.html",
+    inject : "body"
+})
+
+
+module.exports = {
+    entry : __dirname + "/src/app.js",
+    output : {
+        path : __dirname + "/dist",
+        filename : "bundle.js"
+    },
+    module : {
+        rules : [
+            {
+                test : /\.js$/,
+                use : "babel-loader",
+                exclude : /node_modules/
+            }
+        ]
+    },
+    mode : "development",
+    plugins : [HtmlPlugin]
+}
