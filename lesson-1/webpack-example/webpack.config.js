fac22e480749f81bca47c34a57e832b1ee3eb95f
lfdantoni <lfdantoni@gmail.com>
2019-03-22 19:53:15 -0300

    Added watch setting (commented)

--- lesson-1/webpack-example/webpack.config.js
+++ lesson-1/webpack-example/webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     mode: 'production',
+    // watch: true, -> set webpack in mode 'watch' changes
     devServer: {
         contentBase: path.resolve(__dirname, 'dist')
     },
