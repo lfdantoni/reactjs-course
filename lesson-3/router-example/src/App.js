b129a482a5385ea4419418d4dfb25ce3c8dd03aa
lfdantoni <lfdantoni@gmail.com>
2019-04-04 01:08:01 -0300

    more changes to lesson-3

--- lesson-3/router-example/src/App.js
+++ lesson-3/router-example/src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
-import './App.css';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import NewReleases from '@material-ui/icons/NewReleases';
 import {BrowserRouter, HashRouter, Link, NavLink, Route, Switch, Redirect} from 'react-router-dom';
 import { Grid, TextField } from '@material-ui/core';
+
+const styles = {
+  active: {
+    color: "red"
+  },
+  link: {
+    marginLeft: "20px"
+  }
+}
 
 class App extends Component {
   render() {
@@ -12,17 +20,18 @@ class App extends Component {
       <HashRouter> {/* routes are like mipage.com/#/login */}
         <Link to="/contacto">Contacto <AccountCircle /> </Link> {/* that represents a simple link */}
 
-        <NavLink to="/login" activeStyle={{ color: "red" }}>Login</NavLink> {/* that allows to add an active style */}
-        <NavLink to="/noticias" activeStyle={{ color: "pink" }}>Noticias <NewReleases /></NavLink>
-        <NavLink to="/redirect">Redirect</NavLink>
-        <NavLink to="/withParam/lfdantoni">With Param</NavLink>
-        <NavLink to="/childrenProp">With Children</NavLink>
+        <NavLink to="/login" style={styles.link} activeStyle={styles.active}>Login</NavLink> {/* that allows to add an active style */}
+        <NavLink to="/noticias" style={styles.link} activeStyle={styles.active}>Noticias <NewReleases /></NavLink>
+        <NavLink to="/redirect" style={styles.link} activeStyle={styles.active}>Redirect</NavLink>
+        <NavLink to="/withParam/lfdantoni" style={styles.link} activeStyle={styles.active}>With Param</NavLink>
+        <NavLink to="/childrenProp" style={styles.link} activeStyle={styles.active}>With Children</NavLink>
 
         <Switch> {/* this allows to put a default component */}
           <Route path="/contacto" component={Contacto} /> {/* that component will load a component to a specific path */}
           <Route path="/login" component={Login} />
           <Route path="/noticias" component={Noticias} />
           <Route path="/redirect" component={RedirectToHome} />
+          <Route path="/withParam" exact component={WithoutParam} />
           <Route path="/withParam/:user" component={WithParam} />
           <Route path="/childrenProp" render={(props) => <ChildrenProp config={3} {...props}/>} /> {/* render/children allows to pass some props to the route component */}
           <Route component={Home} /> {/* this component represent the dafault one */}
@@ -73,6 +82,9 @@ export const RedirectToHome = () => {
 } 
 export const WithParam = ({match, location, history}) => {
   return <h1>With param {match.params.user}</h1>
+}
+export const WithoutParam = ({match, location, history}) => {
+  return <h1>Without param {match.params.user}</h1>
 }
 export const ChildrenProp = () => {
   return <h1>Pagina de Children Prop</h1>
