f661dcc4361f07e5fab9d77bf466e340e1812b0a
lfdantoni <lfdantoni@gmail.com>
2019-09-04 20:18:08 -0300

    lesson-1 added react project vanilla

---
+++ lesson-1/react-vanilla/src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {render} from 'react-dom';
+import styles from './app.css';
+
+render(
+    <h1 className="title">TEST APPw</h1>,
+    document.getElementById('root')
+);
