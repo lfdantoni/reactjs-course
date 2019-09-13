7d6daa861ac081c03269b8fbd4bf27a6e3a0c04f
lfdantoni <lfdantoni@gmail.com>
2019-09-12 22:13:48 -0300

    lesson-2 added uplifting

--- lesson-2/state-props/src/components/State/index.js
+++ lesson-2/state-props/src/components/State/index.js
@@ -1,12 +1,20 @@
 import React, {Component} from 'react';
 import styles from './styles.module.css';
+import {StateChild} from './StateChild'
 
 export class State extends Component {
     state = {
         myStateValue: 'test value'
     }
 
+    needMoney = (count) => {
+        console.log('It needs $' + count + ' ' + this.state.myStateValue);
+    }
+
     render() {
-        return <h2 className={styles.title}>State component: {this.state.myStateValue}</h2>
+        return <h2 className={styles.title}>
+            State component: {this.state.myStateValue}
+            <StateChild callback={this.needMoney} />
+        </h2>
     }
 }
