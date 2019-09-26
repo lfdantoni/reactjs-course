06bb7f38ed322e3c6b804bd95722416774127849
lfdantoni <lfdantoni@gmail.com>
2019-09-25 21:37:46 -0300

    final-project added api call

--- final-project/src/components/Detail/index.js
+++ final-project/src/components/Detail/index.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import data from '../../mock';
+import { CircularProgress } from '@material-ui/core';
 
-export default function Detail({match}) {
-  const pokemons = data.filter(pok => pok.id.toString() === match.params.id.toString());
-
-  if (pokemons.length === 0) {
-    return <h1>Pokemon not found</h1>
+export default class Detail extends React.Component {
+  state = {
+    pokemon: null,
+    error: null
   }
 
-  const pokemon = pokemons[0];
+  getTypes = (types) => {
+    if(types.length > 1) {
+      return types.reduce((previous, current) => previous.type.name + ', ' + current.type.name);
+    }
 
-  const idStr = pokemon.id.toString();
-  const pIdStr = '000'.substring(0, 3 - idStr.length) + idStr
+    return types[0].type.name;
+  }
 
-  return (<div>
-    <img src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${pIdStr}.png`} alt="test"></img>
-    <br />
-    ID: {pokemon.id}
-    <br />
-    Name: {pokemon.name}
-    <br />
-    Types: {pokemon.typeList.join(', ')}
+  render() {
 
-  </div>)
+    if (this.state.error) {
+      return <h1>Pokemon not found</h1>;
+    }
+
+    if (this.state.pokemon === null) {
+      return <CircularProgress />;
+    }
+
+    const {pokemon} = this.state;
+
+    const idStr = pokemon.id.toString();
+    const pIdStr = '000'.substring(0, 3 - idStr.length) + idStr
+
+    return <div key={idStr}>
+      <img src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${pIdStr}.png`} alt="test"></img>
+      <br />
+      ID: {pokemon.id}
+      <br />
+      Name: {pokemon.name}
+      <br />
+      Types: {this.getTypes(pokemon.types)}
+
+    </div>
+  }
+
+  componentDidMount() {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${this.props.match.params.id}`)
+      .then(resp => resp.json())
+      .then(jsonData => this.setState({pokemon: jsonData}))
+      .catch(error => this.setState({error}))
+  }
 }
--- final-project/src/components/ListCards/index.js
+++ final-project/src/components/ListCards/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import data from '../../mock';
 import PokemonCard from '../PokemonCard';
 import Search from '../Search';
 import Grid from '@material-ui/core/Grid';
+import { CircularProgress } from '@material-ui/core';
 
 const useStyles = {
   root: {
@@ -16,31 +16,60 @@ const useStyles = {
 
 export default class ListCards extends React.Component {
   state = {
-    pokemon: ''
+    pokemon: '',
+    loading: false,
+    pokemons: [],
+    pokemonsFiltered: []
+  }
+
+  showList = () => {
+    return <>
+      <Search onChange={this.onChange} style={useStyles.search} />
+      <Grid container spacing={3}>
+        {
+          this.state.pokemonsFiltered
+            .map(elem => (
+              <Grid item xs={6} lg={3} key={elem.name}>
+                <PokemonCard data={elem}/>
+              </Grid>))
+        }
+      </Grid>
+    </>
+  }
+
+  filter = () => {
+    const pokemonsFiltered = this.state.pokemons
+      .filter(
+        pokemon =>
+          this.state.pokemon &&
+          ( pokemon.id === this.state.pokemon ||
+            pokemon.name.toLowerCase().indexOf(this.state.pokemon.toLowerCase()) >= 0));
+
+    this.setState({pokemonsFiltered, loading: false});
   }
 
   onChange = (value) => {
-    this.setState({ pokemon: value })
+    this.setState({ pokemon: value, loading: true }, () => this.filter());
   }
 
   render() {
     return (
       <div style={useStyles.root}>
-        <Search onChange={this.onChange} style={useStyles.search} />
-        <Grid container spacing={3}>
-          {
-            data
-              .filter(
-                pokemon =>
-                  this.state.pokemon &&
-                  ( pokemon.id.toString() === this.state.pokemon ||
-                    pokemon.name.toLowerCase().indexOf(this.state.pokemon.toLowerCase()) >= 0))
-              .map(elem => (
-                <Grid item xs={3} key={elem.id}>
-                  <PokemonCard data={elem}/>
-                </Grid>))
-          }
-        </Grid>
+        {this.state.loading ? 
+          <CircularProgress /> :
+          this.showList()}
       </div>)
+  }
+
+  componentDidMount() {
+    this.setState({loading: true},
+      () => {
+        fetch(`https://pokeapi.co/api/v2/pokemon?limit=807`)
+        .then(resp => resp.json())
+        .then(jsonData => {
+          jsonData.results.forEach(elem => elem.id = elem.url.match(/\/(\d+)\//)[0].replace(/\//gi,''))
+          this.setState({pokemons: jsonData.results, loading: false})
+        });
+      })
   }
 }
