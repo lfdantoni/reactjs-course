fbb74b02bb7940892fb23b790ca6e268a3304146
Leonardod <lfdantoni@gmail.com>
2020-05-13 21:51:42 -0300

    Revert "add meme gen material ui"
    This reverts commit f77a4f4df1a4ca68b29bcb0809258e493bee71e3.

--- meme-generator/src/components/CreateMeme/index.jsx
+++
@@ -1,7 +0,0 @@
-import React, { Fragment } from 'react';
-
-export default function CreateMeme() {
-  return <Fragment>
-    test create
-  </Fragment>
-}
--- meme-generator/src/components/MyMemes/index.jsx
+++
@@ -1,7 +0,0 @@
-import React, { Fragment } from 'react';
-
-export default function MyMemes() {
-  return <Fragment>
-    test my memes
-  </Fragment>
-}
