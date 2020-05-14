1bdf09d9c4b5d26c38c61b96713312dd288f9f76
Leonardod <lfdantoni@gmail.com>
2020-05-13 21:33:13 -0300

    add meme gen material ui

--- meme-generator/src/App.jsx
+++ meme-generator/src/App.jsx
@@ -1,13 +1,53 @@
 import React from 'react';
 import './App.css';
 import mock from './mock_data';
+import Menu from './components/Menu';
+import Header from './components/Header';
+import Content from './components/Content';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { CssBaseline, Grid, Container } from '@material-ui/core';
+import { PureComponent } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      {mock.data.memes[0].url}
-    </div>
-  );
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+});
+
+export default class App extends PureComponent {
+  defaultMenuId = "memes";
+
+  state = {
+    activeMenu: this.defaultMenuId
+  }
+  
+  menuChanged = (menuId) => {
+    console.log('Menu changed: ' + menuId);
+
+    this.setState({activeMenu: menuId});
+  }
+
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <CssBaseline /> {/** this is to apply the correct theme color */}
+
+        <Container maxWidth={false}>
+
+          <Grid container spacing={2} direction="column">
+            <Grid item>
+              <Header />
+            </Grid>
+            <Grid item>
+              <Menu menuChange={this.menuChanged} defaultMenuId={this.defaultMenuId} />
+            </Grid>
+            <Grid item>
+              <Content menuSelected={this.state.activeMenu}/>
+            </Grid>
+          </Grid>
+
+        </Container>
+      </ThemeProvider>
+    );
+  }
 }
-
-export default App;
