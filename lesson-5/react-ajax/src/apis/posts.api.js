7bab8b2a363d87832976fed1d40e4a60b266f5a5
Leonardod <lfdantoni@gmail.com>
2020-05-26 21:01:11 -0300

    lesson-5 added create post example

--- lesson-5/react-ajax/src/apis/posts.api.js
+++ lesson-5/react-ajax/src/apis/posts.api.js
@@ -4,3 +4,18 @@ export const getPosts = () => {
   return fetch(`${API_URL}/posts`)
     .then(response => response.json());
 }
+
+export const addPost = (userId, title, body) => {
+  return fetch(`${API_URL}/posts`, {
+    method: 'POST',
+    body: JSON.stringify({
+      title,
+      body,
+      userId
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+  .then(response => response.json())
+}
--- lesson-5/react-ajax/src/components/PostList/index.js
+++ lesson-5/react-ajax/src/components/PostList/index.js
@@ -1,13 +1,17 @@
 import React, { PureComponent } from 'react'
-import { getPosts } from '../../apis/posts.api'
+import { getPosts, addPost } from '../../apis/posts.api'
 import PostCard from './PostCard';
 import styles from './styles.module.css';
 import { getRandomPostImage } from '../../utils/images';
 import { randomDate } from '../../utils/dates';
+import { Fragment } from 'react';
 
 export default class PostList extends PureComponent {
   state = {
-    posts: []
+    posts: [],
+    title: '',
+    userId: '',
+    body: ''
   }
 
   fillMissInfo = (posts) => {
@@ -36,9 +40,35 @@ export default class PostList extends PureComponent {
       })
   }
 
+  handleInput = (e, property) => {
+    this.setState({[property]: e.target.value})
+  }
+
+  createPost = () => {
+    addPost(this.state.userId, this.state.title, this.state.body)
+      .then((resp) => {
+        console.log(resp);
+        this.setState({
+          userId: '',
+          title: '',
+          body: ''
+        })
+      });
+  }
+
+  getCreatePostView() {
+    return <div className={styles['create-post']}>
+      <input type="text" placeholder="UserId" value={this.state.userId} onChange={(e) => this.handleInput(e, 'userId')}/>
+      <input type="text" placeholder="Title" value={this.state.title} onChange={(e) => this.handleInput(e, 'title')}/>
+      <input type="text" placeholder="Body" value={this.state.body} onChange={(e) => this.handleInput(e, 'body')}/>
+      <button onClick={this.createPost}>Create Post</button>
+    </div>
+  }
+
   render() {
     return (
       <div className={styles.list}>
+        {this.getCreatePostView()}
         {!this.state.posts.length ? this.getLoadingView() : this.getListView()}
       </div>
     )
