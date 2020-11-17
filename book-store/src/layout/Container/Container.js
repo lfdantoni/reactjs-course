69cc8dd1757e819ad8d0d2f45e7c833807cce645
lfdantoni <lfdantoni@gmail.com>
2020-11-17 00:17:03 -0300

    Add react-router and pages

--- book-store/src/layout/Container/Container.js
+++ book-store/src/layout/Container/Container.js
@@ -1,20 +1,28 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import MatContainer from '@material-ui/core/Container'
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import { BookList } from '../../pages/BookList';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
-import MatContainer from '@material-ui/core/Container'
+import { AddBook } from '../../pages/AddBook';
+import { NotFound } from '../../pages/NotFound';
+
 import './container.css';
 
 export const Container = () => {
   return (
-    <Fragment>
+    <BrowserRouter>
       <Header/>
       
       <MatContainer maxWidth="lg" className="content">
-        <BookList />
+        <Switch>
+          <Route path="/add" component={AddBook} />
+          <Route path="/" exact component={BookList} />
+          <Route component={NotFound} />
+        </Switch>
       </MatContainer>
 
       <Footer/>
-    </Fragment>
+    </BrowserRouter>
   )
 }
---
+++ book-store/src/pages/AddBook/AddBook.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const AddBook = () => {
+  return (
+    <div>
+      Add Book Form
+    </div>
+  )
+}
