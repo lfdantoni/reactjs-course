6b509342416b6701aa9ee8058b221c97563855a8
lfdantoni <lfdantoni@gmail.com>
2020-12-18 22:25:44 -0300

    -

--- lessons/redux-example/index.js
+++ lessons/redux-example/index.js
@@ -97,6 +97,11 @@ const reducers = combineReducers({
   user
 })
 
+// const store = createStore(
+//   todos,
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+
 const store = createStore(
   reducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
