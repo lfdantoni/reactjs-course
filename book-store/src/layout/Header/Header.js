f37cff06e3841d186881a7886e03f2a093047ff2
lfdantoni <lfdantoni@gmail.com>
2021-05-29 13:02:58 -0300

    add links

--- book-store/src/layout/Header/Header.js
+++ book-store/src/layout/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Grid } from '@material-ui/core';
 import MatContainer from '@material-ui/core/Container';
+import { Link } from 'react-router-dom';
 
 import styles from './header.module.css';
 
@@ -17,7 +18,7 @@ export const Header = () => {
       <MatContainer maxWidth="lg">
         <Grid container spacing={0} className={styles['header-wrapper']} alignItems="center">
           <Grid item xs={2}>
-            {/* <Link to="/">Book List</Link> */}
+            <Link to="/">Book List</Link>
           </Grid>
           <Grid item xs={2}>
           </Grid>
